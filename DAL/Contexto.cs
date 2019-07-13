f99b2292dddfcc6fb2286db0bcb716012b8779ad
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-12 20:46:07 -0400

    Inicio del proyecto final.

---
+++ DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Ususarios { get; set; }
+        public Contexto() : base("DbProyectoFinal-WalderReyes")
+        {
+
+        }
+    }
+}
