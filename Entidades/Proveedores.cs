0fcf0a4a0f510626de1ab0949e3662e7d04610de
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-16 23:48:03 -0400

    Creacion de la clase Proveedores.

---
+++ Entidades/Proveedores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Proveedores
+    {
+        public int ProveedorId { get; set; }
+        public string NombreProveedor { get; set; }
+        public string RNC { get; set; }
+        public string TelefonoProveedor { get; set; }
+        public string Email { get; set; }
+        public string NombreRepresentante { get; set; }
+        public string EstencionRepresentante { get; set; }
+        public Proveedores()
+        {
+            ProveedorId = 0;
+            NombreProveedor = string.Empty;
+            RNC = string.Empty;
+            TelefonoProveedor = string.Empty;
+            Email = string.Empty;
+            NombreRepresentante = string.Empty;
+            EstencionRepresentante = string.Empty;
+
+        }
+
+        public Proveedores(int proveedorId, string nombreProveedor, string rNC, string telefonoProveedor, string email, string nombreRepresentante, string estencionRepresentante)
+        {
+            ProveedorId = proveedorId;
+            NombreProveedor = nombreProveedor;
+            RNC = rNC;
+            TelefonoProveedor = telefonoProveedor;
+            Email = email;
+            NombreRepresentante = nombreRepresentante;
+            EstencionRepresentante = estencionRepresentante;
+        }
+    }
+}
