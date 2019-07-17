6175b34e079021a0c915052fdf4b67faa2975755
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-16 23:46:40 -0400

    Creacion de la entidad Productos.

---
+++ Entidades/Productos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Productos
+    {
+        public int ProductoId { get; set; }
+        public int ProveedoresId { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Cantidad { get; set; }
+        public decimal Costo { get; set; }
+        public decimal Precio { get; set; }
+        public decimal Ganancia { get; set; }
+        public decimal ITebis { get; set; }
+
+
+        public Productos()
+        {
+            ProductoId = 0;
+            ProveedoresId = 0;
+            Descripcion = string.Empty;
+            Cantidad = 0;
+            Costo = 0;
+            Precio = 0;
+            Ganancia = 0;
+        }
+        public Productos(int productoId, int proveedores, string descripcion, decimal cantidad, decimal costo, decimal precio, decimal ganancia)
+        {
+            ProductoId = productoId;
+            ProveedoresId = proveedores;
+            Descripcion = descripcion;
+            Cantidad = cantidad;
+            Costo = costo;
+            Precio = precio;
+            Ganancia = ganancia;
+        }
+    }
+}
