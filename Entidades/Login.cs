c20395d3793c68cc8a3482e5132323732dd3048c
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-16 23:50:42 -0400

    Creacion de la entidad Login

---
+++ Entidades/Login.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Login
+    {
+        public int LoginId { get; set; }
+        public string Nombre { get; set; }
+        public string Constraseña { get; set; }
+
+        public Login()
+        {
+            LoginId = 0;
+            Nombre = string.Empty;
+            Constraseña = string.Empty;
+
+        }
+        public Login(int logingId, string nombre, string constraseña)
+        {
+            LoginId = logingId;
+            Nombre = nombre;
+            Constraseña = constraseña;
+        }
+    }
+}
