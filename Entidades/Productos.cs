98d6132a5c8d4a6a497a9935403acf67a9070c82
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-21 17:15:03 -0400

    Tests del la entidad productos

--- Entidades/Productos.cs
+++ Entidades/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace Entidades
 {
     public class Productos
     {
+        [Key]
         public int ProductoId { get; set; }
         public int ProveedoresId { get; set; }
         public int CategoriaId { get; set; }
---
+++ EntidadesTests/ProductosTests.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using DAL;
+
+namespace Entidades.Tests
+{
+    [TestClass()]
+    public class ProductosTests
+    {
+        [TestMethod()]
+        public void Guardar()
+        {
+            RepositorioBase<Productos> reposistorio = new RepositorioBase<Productos>(new Contexto());
+            Assert.IsTrue(reposistorio.Guardar(new Productos()));
+        }
+
+        [TestMethod()]
+        public void Buscar()
+        {
+            RepositorioBase<Productos> reposistorio = new RepositorioBase<Productos>(new Contexto());
+            Assert.IsNotNull(reposistorio.Buscar(1));
+        }
+
+        [TestMethod()]
+        public void GetList()
+        {
+            RepositorioBase<Productos> reposistorio = new RepositorioBase<Productos>(new Contexto());
+            Assert.IsNotNull(reposistorio.GetList(a => true));
+        }
+
+
+        [TestMethod()]
+        public void Modificar()
+        {
+            RepositorioBase<Productos> reposistorio = new RepositorioBase<Productos>(new Contexto());
+            Productos producto = new Productos();
+            producto.ProductoId = 1;
+            producto.Descripcion = "Azucar";
+            producto.ProveedoresId = 1;
+            producto.Cantidad = 1;
+            producto.Costo = 1;
+            producto.Precio = 1;
+            producto.Itebis = 1;
+            producto.Ganancia = 10;
+            producto.CategoriaId = 1;
+
+            Assert.IsTrue(reposistorio.Modificar(producto));
+        }
+
+        [TestMethod()]
+        public void Eliminar()
+        {
+            RepositorioBase<Productos> reposistorio = new RepositorioBase<Productos>(new Contexto());
+            Assert.IsNotNull(reposistorio.Eliminar(1));
+        }
+
+    }
+}
