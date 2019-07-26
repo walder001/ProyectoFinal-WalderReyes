e8e4610427fbabdde53684c608f387151e83bdf8
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-25 20:25:38 -0400

    Entidad compra realizada.

---
+++ Entidades/Compra.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Compra
+    {
+        public int CompraId { get; set; }
+        public int UsuarioId { get; set; }
+        public int ProductoId { get; set; }
+        public int ProveedorId { get; set; }
+        public DateTime FechaCompra { get; set; }
+        public decimal TotalCompra { get; set; }
+        public Compra()
+        {
+            CompraId = 0;
+            UsuarioId = 0;
+            ProductoId = 0;
+            ProveedorId = 0;
+            FechaCompra = DateTime.Now;
+            TotalCompra = 0;
+        }
+
+        public Compra(int compraId, int usuarioId, int productoId, int proveedorId, DateTime fechaCompra, decimal totalCompra)
+        {
+            CompraId = compraId;
+            UsuarioId = usuarioId;
+            ProductoId = productoId;
+            ProveedorId = proveedorId;
+            FechaCompra = fechaCompra;
+            TotalCompra = totalCompra;
+        }
+    }
+}
