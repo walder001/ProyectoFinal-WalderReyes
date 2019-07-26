77fda71db5945d984172058a4c972d3ea35d744b
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-25 21:33:45 -0400

    Interfaz de la entidad categoria

---
+++ ProyectoFinal-WalderReyes/UI/Registro/rCategorias.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ProyectoFinal_WalderReyes.UI.Registro
+{
+    partial class rCategorias
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rCategorias));
+            this.CategoriaIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnNuevo = new System.Windows.Forms.Button();
+            this.NombreCategoriaTextBox = new System.Windows.Forms.TextBox();
+            this.nombres = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriaIdNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CategoriaIdNumericUpDown
+            // 
+            this.CategoriaIdNumericUpDown.Location = new System.Drawing.Point(142, 24);
+            this.CategoriaIdNumericUpDown.Name = "CategoriaIdNumericUpDown";
+            this.CategoriaIdNumericUpDown.Size = new System.Drawing.Size(98, 20);
+            this.CategoriaIdNumericUpDown.TabIndex = 145;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.Color.White;
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnBuscar.Location = new System.Drawing.Point(259, 15);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(104, 32);
+            this.btnBuscar.TabIndex = 144;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Image = ((System.Drawing.Image)(resources.GetObject("btnGuardar.Image")));
+            this.btnGuardar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnGuardar.Location = new System.Drawing.Point(142, 115);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(98, 32);
+            this.btnGuardar.TabIndex = 143;
+            this.btnGuardar.Text = "Guargar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Image = ((System.Drawing.Image)(resources.GetObject("btnEliminar.Image")));
+            this.btnEliminar.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEliminar.Location = new System.Drawing.Point(266, 115);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(97, 32);
+            this.btnEliminar.TabIndex = 142;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.BackColor = System.Drawing.SystemColors.Highlight;
+            this.btnNuevo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNuevo.ForeColor = System.Drawing.Color.White;
+            this.btnNuevo.Image = ((System.Drawing.Image)(resources.GetObject("btnNuevo.Image")));
+            this.btnNuevo.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNuevo.Location = new System.Drawing.Point(19, 115);
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(94, 32);
+            this.btnNuevo.TabIndex = 141;
+            this.btnNuevo.Text = "Nuevo";
+            this.btnNuevo.UseVisualStyleBackColor = false;
+            // 
+            // NombreCategoriaTextBox
+            // 
+            this.NombreCategoriaTextBox.Location = new System.Drawing.Point(142, 65);
+            this.NombreCategoriaTextBox.Name = "NombreCategoriaTextBox";
+            this.NombreCategoriaTextBox.Size = new System.Drawing.Size(221, 20);
+            this.NombreCategoriaTextBox.TabIndex = 140;
+            // 
+            // nombres
+            // 
+            this.nombres.AutoSize = true;
+            this.nombres.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nombres.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.nombres.Location = new System.Drawing.Point(28, 72);
+            this.nombres.Name = "nombres";
+            this.nombres.Size = new System.Drawing.Size(50, 13);
+            this.nombres.TabIndex = 135;
+            this.nombres.Text = "Nombre";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(28, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 134;
+            this.label1.Text = "CategoriaId";
+            // 
+            // rCategorias
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(390, 170);
+            this.Controls.Add(this.CategoriaIdNumericUpDown);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnNuevo);
+            this.Controls.Add(this.NombreCategoriaTextBox);
+            this.Controls.Add(this.nombres);
+            this.Controls.Add(this.label1);
+            this.Name = "rCategorias";
+            this.Text = "rCategorias";
+            ((System.ComponentModel.ISupportInitialize)(this.CategoriaIdNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown CategoriaIdNumericUpDown;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnNuevo;
+        private System.Windows.Forms.TextBox NombreCategoriaTextBox;
+        private System.Windows.Forms.Label nombres;
+        private System.Windows.Forms.Label label1;
+    }
+}
