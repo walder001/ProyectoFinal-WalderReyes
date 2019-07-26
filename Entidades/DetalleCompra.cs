409edb70a40b0250072b6df543a5a8575eccf43a
Walder Reyes De Jesus <walder_reyes@ucne.edu.do>
2019-07-25 20:32:44 -0400

    Entidad detalle compra

---
+++ Entidades/DetalleCompra.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DetalleCompra
+    {
+        public int DetalleCompraId { get; set; }
+        public int CompraId { get; set; }
+        public int ProductoId { get; set; }
+        public decimal Catidad { get; set; }
+        public decimal Costo { get; set; }
+        public decimal TotalCompra { get; set; }
+        public DetalleCompra()
+        {
+            DetalleCompraId = 0;
+            CompraId = 0;
+            ProductoId = 0;
+            Catidad = 0;
+            Costo = 0;
+            TotalCompra = 0;
+
+
+        }
+
+    }
+}
